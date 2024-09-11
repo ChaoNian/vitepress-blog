46af0e9c45739a1f1ae07a031708ad2652f75d7a
huanghao <1063462242@qq.com>
2024-09-11 23:32:48 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "WebWong 的技术记录",
+  description: "A VitePress Site",
+  themeConfig: {
+    // 自定义上次更新的文本和日期格式
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    logo:'/logo.png',
+
+    // 文章目录设置
+    outlineTitle: '目录',
+    outline: [2,6],
+    // https://vitepress.dev/reference/default-theme-config
+    // 导航
+    nav: [
+      // { 
+      //   text: '更多',
+      //   items: [
+      //     {text: 'Home', link: '/' },
+      //     {text: 'Home',link: '/'  }
+      //   ]
+      // },
+      { text: 'Home', link: '/' },
+      { text: '前端', link: '/markdown-examples' }
+    ],
+
+    // 侧边栏
+    sidebar: [
+      {
+        text: '设计模式',
+        items: [
+          { text: '面向对像', link: '/markdown-examples' },
+          { text: '工厂模式', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    // 外链
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ChaoNian' }
+    ],
+
+    // 页脚
+    footer: {
+      copyright: 'Copyright@ 2024 WebWong'
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "前端技术"
+  # text: "探索记录"
+  tagline: 好奇这是一项什么技术？
+  image: 
+     src: /background.svg
+     alt: 背景图片
+  actions:
+    - theme: brand
+      text: 前端博客
+      link: /markdown-examples
+    # - theme: alt
+      # text: API Examples
+      # link: /api-examples
+
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+# ---
+
