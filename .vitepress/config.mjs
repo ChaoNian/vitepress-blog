b3a537560d887eca033e9439b324dc23aa68600e
NorlangWong <44801725+ChaoNian@users.noreply.github.com>
2024-10-06 08:44:53 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/vitepress-blog/',
   title: "WebWong 的技术记录",
   description: "A VitePress Site",
   themeConfig: {
